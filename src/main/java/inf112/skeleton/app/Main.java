67d0bc16feb5e291e4d26b2eb60342b57fced3c8
syregutt <sigurd_aleksander1999@hotmail.com>
2020-02-05 09:29:26 +0100

    Made map 4 layers instead of 10.
    Now the map has 4 layers; board, walls, interactables, player_start
    
    Board: Base layer
    Walls: Walls that block movemement
    Interactables: Lasers, movers, repairs, holes
    PlayerStart: Starting position for robots.

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -10,8 +10,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally la Ingenting";
-        cfg.width = 640;
-        cfg.height = 640;
+        cfg.width = 800;
+        cfg.height = 800;
         Game game = new Game();
         new LwjglApplication(new HelloWorld(), cfg);
     }
