45879f495f8d6fe2fa3c7186b6601fdae5c20a0d
syregutt <sigurd_aleksander1999@hotmail.com>
2020-02-06 16:50:38 +0100

    Added player-icon for testing.
    Temporary player icon added for testing. New player icons will be added later.

--- src/main/java/inf112/ingenting/roborally/Launcher.java
+++ src/main/java/inf112/ingenting/roborally/Launcher.java
@@ -4,18 +4,22 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Vector2;
 import inf112.ingenting.roborally.board.Board;
 
+import java.io.File;
 import java.util.Iterator;
 
 public class Launcher extends ApplicationAdapter {
     private OrthographicCamera camera;
 	private SpriteBatch batch;
-
+    private Sprite sprite;
+    private Texture player1;
     private Board gameBoard;
 
     @Override
@@ -24,8 +28,12 @@ public class Launcher extends ApplicationAdapter {
         camera.setToOrtho(false, 12, 12);
         camera.zoom = 1f; // To be added when cards are added.
         camera.update();
-
         gameBoard = new Board("mainMap.tmx", (float) 1 / 64, camera);
+
+        batch = new SpriteBatch();
+        player1 = new Texture(Gdx.files.internal("player.png"));
+        sprite = new Sprite(player1);
+
 
     }
 
@@ -37,7 +45,12 @@ public class Launcher extends ApplicationAdapter {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
         gameBoard.render();
+
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
     }
 
     @Override
