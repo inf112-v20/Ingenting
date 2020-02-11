51bc84fccce2733d3e92e223253ddc20633586a5
Jakob Snorrason <jak.snorrason@gmail.com>
2020-02-11 15:04:27 +0000

    Added a cardtype enum
    The enum is to be used to create programming cards. Card priorities are currently hardcoded, but will likely later be randomly generated when needed.

---
+++ src/main/java/inf112/ingenting/roborally/cards/CardType.java
@@ -0,0 +1,49 @@
+package inf112.ingenting.roborally.cards;
+
+import inf112.ingenting.roborally.board.MoveType;
+
+import static inf112.ingenting.roborally.board.MoveType.FORWARD;
+
+/**
+ * Enum for types of cards
+ *
+ * A list of moves corresponding to the enum value can be returned
+ * by the <code>getMoves</code> function.
+ *
+ * The <code>getPriority</code> function will most likely be removed
+ * in a later version of the program, but for now it is used for
+ * testing purposes.
+ */
+public enum CardType {
+	MOVE_1_P1(FORWARD, 630),
+	MOVE_1_P2(FORWARD, 550),
+	MOVE_2(new MoveType[]{FORWARD, FORWARD}, 740),
+	MOVE_3(new MoveType[]{FORWARD, FORWARD, FORWARD}, 840),
+	ROTATE_RIGHT_P1(MoveType.ROTATE_RIGHT, 400),
+	ROTATE_RIGHT_P2(MoveType.ROTATE_RIGHT, 260),
+	ROTATE_RIGHT_P3(MoveType.ROTATE_RIGHT, 120),
+	ROTATE_LEFT_P1(MoveType.ROTATE_LEFT, 300),
+	ROTATE_LEFT_P2(MoveType.ROTATE_LEFT, 240),
+	ROTATE_LEFT_P3(MoveType.ROTATE_LEFT, 100);
+
+	private final MoveType[] moves;
+	private final int priority;
+
+	CardType(MoveType move, int priority) {
+		this.moves = new MoveType[]{move};
+		this.priority = priority;
+	}
+
+	CardType(MoveType[] moves, int priority) {
+		this.moves = moves;
+		this.priority = priority;
+	}
+
+	public MoveType[] getMoves() {
+		return moves;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+}
