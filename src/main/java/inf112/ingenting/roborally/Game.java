306014a56fb1a3fade08c921e4adca811bcf2b32
Ayoub Tammaoui <45130580+Tammaoui@users.noreply.github.com>
2020-02-11 14:51:42 +0100

    Added Game class to handle round logic and rendering. Also added Player class to represent players.

---
+++ src/main/java/inf112/ingenting/roborally/Game.java
@@ -0,0 +1,30 @@
+package inf112.ingenting.roborally;
+
+import com.badlogic.gdx.utils.Timer;
+import inf112.ingenting.roborally.board.Board;
+import inf112.ingenting.roborally.player.Player;
+
+public class Game {
+    private Board board; // Game board.
+    private Player[] players; //Active players in game.
+
+    public Game(Board board, Player[] players){
+        this.board = board;
+        this.players = players;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void gameRound(){
+        board.render();
+        // TODO: Add round logic.
+        board.render();
+    }
+
+}
